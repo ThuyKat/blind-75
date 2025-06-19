e350d5d88e65cf28cfb30d2543850fa4cdb11ce8
Thuy Katie Nguyen <katiefirst7@gmail.com>
2025-06-20 01:03:43 +1000

    update

--- leetcode/length-of-last-word.js
+++ leetcode/length-of-last-word.js
@@ -1,26 +1,9 @@
     /**
- * @param {number[]} digits
- * @return {number[]}
+ * @param {string} s
+ * @return {number}
  */
-var plusOne = function(digits) {
-    // go from the end of digits
-    //the result is (digits[i]+1) % 10 if it's >10 then it's the large integer +10
-    let i = digits.length -1
-    while (i>=0){
-        
-        if(digits[i]+1>=10){
-            digits[i] = (digits[i] +1)%10
-            if(i==0){
-                digits.unshift(1)
-                return digits
-            }
-        }else{
-            digits[i] ++
-            return digits
-        }
-        
-        i--
-        
-    }
-    
+var lengthOfLastWord = function(s) {
+    //string s, return last word of string
+    let arr_s = s.trim().split(" ")
+    return arr_s.pop().length
 };

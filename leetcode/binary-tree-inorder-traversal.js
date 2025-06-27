505463e48cf77b2cad18e1d54d521f1dc075f6d8
Thuy Katie Nguyen <katiefirst7@gmail.com>
2025-06-27 23:57:47 +1000

    update

--- leetcode/binary-tree-inorder-traversal.js
+++ leetcode/binary-tree-inorder-traversal.js
@@ -11,5 +11,10 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
-    
+      //inorder --> left branch, root, right branch
+    //first number is root, then second row 1*2 = 2 numbers
+    //then third row that has 2*2=4 numbers - 1 null = 3 numbers
+    // then fourth rows has remaining 
+    //k --> 2k-null --> (2k-null)*2 -null
+    // output : go from last row, beginning of row is null, move to 1 level obove it--> push in output --> next one is second value of last row --> 
 };

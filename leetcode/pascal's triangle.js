80754209bf3fb7ad40bfed762c858e73c6573e76
Thuy Katie Nguyen <katiefirst7@gmail.com>
2025-06-28 22:01:52 +1000

    add solution for pascal's triangle

---
+++ leetcode/pascal's triangle.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+  if(numRows==1){
+    return [[1]]
+  }
+  if(numRows==2){
+    return[[1],[1,1]]
+  }
+  if(numRows>2){
+    let result = generate(numRows-1)
+    let lastEl = result[numRows-2]
+    let newEl =[1]
+    for(let i=0;i<lastEl.length-1;i++){
+        newEl.push(lastEl[i]+lastEl[i+1])
+    }
+    newEl.push(1)
+    result.push(newEl)
+    return result
+  }
+};

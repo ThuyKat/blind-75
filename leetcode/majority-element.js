86a81995199c856724f7749160f5aeddf24619ba
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-01-13 23:56:06 +1100

    easy-majority-element solution

---
+++ leetcode/majority-element.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    
+};
+// majority element always exist, to find it I can sort the array and count the appearance, prob create another array to record frequency, but this wont work for large list
+//to find mode, find median and mean first. To find median, find the max, and the mean. 

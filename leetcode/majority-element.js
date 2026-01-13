b15e41a7b8a5346ad2c0fcb9d21d734d2bb46885
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-01-14 00:10:43 +1100

    update

--- leetcode/majority-element.js
+++ leetcode/majority-element.js
@@ -6,4 +6,4 @@ var majorityElement = function(nums) {
     
 };
 // majority element always exist, to find it I can sort the array and count the appearance, prob create another array to record frequency, but this wont work for large list
-//to find mode, find median and mean first. To find median, find the max, and the mean. 
+//to find mode, find median and mean first. To find median, need to find the number that has n/2 larger than it and n/2 smaller than it. if n is odd, median =mode><=mean. If n is even then median = mode ><=mean, in both case median is mode. 

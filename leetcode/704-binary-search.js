aa59fc544947033a9f7da455fc27f51af7701620
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-01-20 12:19:20 +1100

    add solution for q.704

---
+++ leetcode/704-binary-search.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+let nums = [-1,0,3,5,9,12]
+var search = function(nums, target) {
+    //nums, sorted 
+    //search target in nums, if not exist return -1
+    //0(logn)
+    //find the middle see if target > middle number
+    let len = nums.length/2
+    let target_index = Math.floor(len)
+    if(target_index==0 ){
+        if (nums[target_index] !=target){
+            return -1
+        }else{
+            return 0
+        }
+    }  
+
+    
+    while(Math.floor(len)>0){
+        if(target>nums[target_index]){
+            target_index=Math.ceil(target_index+len/2)
+            
+        }else if(target<nums[target_index]){
+            target_index=Math.floor(target_index-len/2)
+        }else{
+            return target_index
+        }
+        len=len/2
+    }
+    if(target!= nums[target_index]){
+        return -1
+    }else{
+        return target_index
+    }
+    
+    
+};
+console.log(search(nums, 9))
+//what's wrong with my code?
+//when len is less than 1, Math.floor(len) = 0, so target_index will not change anymore
+//need to add a check for that

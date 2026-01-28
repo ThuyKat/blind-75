d671934d34e3ff093dc98733e8c4ef0998544140
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-01-28 23:02:46 +1100

    solution for q.258

---
+++ leetcode/258-add-digit.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+    //extract each digit and sum it up
+    //repeat the process till sum <10
+        if(num<10){
+            return num
+        }
+        while(num>=10){
+            let sum=0
+            while(num>0){
+                sum +=num%10
+                num= (num-num%10)/10
+            }
+            num=sum
+            // num=num.toString().split("").reduce((sum,curr) => sum+Number(curr),0)
+        }
+    return num
+};

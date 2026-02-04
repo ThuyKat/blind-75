10ec0ad9b24f9a29b1f555faa12336378d17f866
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-02-04 23:54:34 +1100

    solution for q.509

---
+++ leetcode/509-Fibonacci-num.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    // if we start from 0, then we got to calculate F(2)
+    // the goal is calculating F(n), so we start from fib(n) = fib(n-1) +fib(n-2)
+    while(n>0){
+        
+    }
+};

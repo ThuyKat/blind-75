064843261ce679c63feb15423f193987d90aa929
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-02-05 00:28:04 +1100

    update solution q.509

--- leetcode/509-Fibonacci-num.js
+++ leetcode/509-Fibonacci-num.js
@@ -5,11 +5,8 @@
 var fib = function(n) {
     // if we start from 0, then we got to calculate F(2)
     // the goal is calculating F(n), so we start from fib(n) = fib(n-1) +fib(n-2)
-    if(n==1){
-        return 1
-    }
-    if(n==0){
-        return 0
+    if(n<=1){
+        return n
     }
     return fib(n-1) + fib(n-2)
 };

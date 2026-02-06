e2510b053a4159b8099aecfc478b515484250ddd
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-02-07 00:16:38 +1100

    add solution for q.1137 - DP exceed the time limit so cant use that

--- leetcode/1137-n-tribonachi.js
+++ leetcode/1137-n-tribonachi.js
@@ -3,5 +3,18 @@
  * @return {number}
  */
 var tribonacci = function(n) {
-    
+    //let sum = T0+T1+T2 --> T3, if we take sum +T3 -T0 we get T4
+    // to keep track of the index so we know where to stop, we use an array
+    if(n<3&&n>0){
+        return 1
+    }
+    if(n==0){
+        return 0
+    }
+    let sum = [0,1,1]
+    while (sum.length<=n){
+        let nextEl = sum[sum.length-1]+sum[sum.length-2]+sum[sum.length-3]
+        sum.push(nextEl)
+    }
+    return sum.at(-1) 
 };

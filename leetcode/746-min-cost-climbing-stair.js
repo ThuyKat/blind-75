40d394c22c8e6b4aa818032972b69a1742a62095
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-02-11 23:21:45 +1100

    add note to solution q.746

--- leetcode/746-min-cost-climbing-stair.js
+++ leetcode/746-min-cost-climbing-stair.js
@@ -6,6 +6,7 @@ var minCostClimbingStairs = function(cost) {
     //cost[i] is the cost of ith step on a stair case
     //one or two steps
     //min cost to the top
+        //to reach index 2, either from index 0 or 1, since cost[0] is smaller first step is from index 0, to reach index 3, either from index 1 or 2
     let n = cost.length;
     let dp = new Array(n + 1).fill(0);
     for (let i = 2; i <= n; i++) {

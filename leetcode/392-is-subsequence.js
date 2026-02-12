25bb9b916754325e5ce9086bb369335f944ca758
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-02-12 23:23:26 +1100

    another approach for q.392

--- leetcode/392-is-subsequence.js
+++ leetcode/392-is-subsequence.js
@@ -9,26 +9,35 @@ var isSubsequence = function(s, t) {
     //find the first char, and then find the second char from the first char position in t
     //we can use split, pick the last element. If after split, only 1 element left, return false
     //if it's subsequent of original, second half of s should be subsequent of second half of t.. keep going to the last char of s
-    if(s.length==1 && t.includes(s[0])){
-        return true
-    }
-    let t_length = t.length
-    console.log(t_length)
-    t = t.split(s[0]).at(-1)
-    s = s.split(s[0]).at(-1)
-    console.log(s,t)
-    console.log(t.length)
-    if(t.length == t.length){
-        return false
-    }
-    if(t.length<s.length){
-        return false
-    }
-    if(!isSubsequence(s,t)){
-        return false
-    }
+    //   if(s.length ==0){
+    //     return true
+    // }
+    // if(s.length==1 && t.includes(s[0])){
+    //     return true
+    // }
+    // let t_length = t.length
+    // let index = t.indexOf(s[0])
+    // t = t.slice(index+1)
+    // s=s.slice(1)
+    // if(t.length == t_length){
+    //     return false
+    // }
+    // if(t.length<s.length){
+    //     return false
+    // }
+    // if(!isSubsequence(s,t)){
+    //     return false
+    // }
     
-    return true
+    // return true
+    //BETTER APPROACH:
+    let pos = -1; // last matched position in t
+  for (let c of s) {
+    pos = t.indexOf(c, pos + 1); // find c after previous match
+    if (pos === -1) return false; // not found later in t
+  }
+  return true; // every char of s found in order
 };
 let s = "abc", t = "ahbgdc"
 console.log(isSubsequence(s,t))
+

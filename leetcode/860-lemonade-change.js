c4ed0007bcc4d819aa4c2bb4f62c32a35048fdb9
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-02-15 00:40:24 +1100

    add thoughts flow

--- leetcode/860-lemonade-change.js
+++ leetcode/860-lemonade-change.js
@@ -3,6 +3,12 @@
  * @return {boolean}
  */
 var lemonadeChange = function(bills) {
+     //each cost $5, bill notes $5,10,or 20. give change
+    //true if enough change
+    //iterate, if need to give change (10 or 20 or !5), find previous bills if sum up to the difference
+    //order is not important, sort notes into a map.. 
+    //issue here is how to get the total sum quickest way. 10 needs 5, 20 needs either 10 and 5, or 3x5
+    //if number of 5 notes is <3 then see number of 10 notes if <1
    let five = 0, ten = 0;
    for (let bill of bills) {
        if (bill === 5) {
@@ -23,4 +29,6 @@ var lemonadeChange = function(bills) {
        }
    }
    return true;
-};
+}
+let bills = [5,5,5,10,20]
+console.log(lemonadeChange(bills))

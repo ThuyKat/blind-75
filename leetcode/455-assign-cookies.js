31f1ed69edb885e8d540930dc93d2fbf06feaa03
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-02-15 23:12:21 +1100

    add q.605

--- leetcode/455-assign-cookies.js
+++ leetcode/455-assign-cookies.js
@@ -14,4 +14,29 @@ var findContentChildren = function(g, s) {
         j++
     }
     return i
+    // g = g.sort((a,b)=> a-b)
+    // s = s.sort((a,b)=> a-b)
+    // console.log(g,s)
+    // let result = 0
+    // for(let i=0;i<g.length;i++){
+    //     let j =0
+    //     while(j<s.length){
+    //         if(s[j]>=g[i]){
+    //             result++
+    //             break
+    //         }
+    //         j++
+    //     }
+    //     if(j<s.length-1){
+    //         s = s.slice(j+1)
+    //         console.log(s)
+    //     }else{
+    //         return result
+    //     }
+    // }
+    // return result
 };
+let g =
+[10,9,8,7,10,9,8,7]
+let s = [5,6,7,8]
+console.log(findContentChildren(g,s))

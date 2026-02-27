f18e3bb86767368f56bb776687027ef018d38cf0
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-02-27 23:25:53 +1100

    add q.94

---
+++ leetcode/94-binary-tree-inorder-traveral.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  let root_arr = [];
+  function bt_arr(node) {
+    if (!node) {
+      return;
+    }
+    bt_arr(node.left);
+    root_arr.push(node.val);
+    bt_arr(node.right);
+  }
+  bt_arr(root);
+  return root_arr;
+};

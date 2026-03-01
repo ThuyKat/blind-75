740b7cd392a0e3afb3ee974ecb1a2cda697c0dc0
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-03-01 23:28:48 +1100

    added explanation for q.144

--- leetcode/144-binary-preorder-traversal.js
+++ leetcode/144-binary-preorder-traversal.js
@@ -11,13 +11,17 @@
  * @return {number[]}
  */
 var preorderTraversal = function (root) {
+  // root has TreeNode structure, and not array structure
+  //to travel, examine from top to bottom
+  // root.val --> push to result array, root.left --> recursion again going dfs until the last element, root.right --> recursion like the left side
+  // it's not a node when node is undefined
   const result = [];
 
   function dfs(node) {
     if (!node) return;
-    result.push(node.val);  // visit root first
-    dfs(node.left);          // then left subtree
-    dfs(node.right);         // then right subtree
+    result.push(node.val); // visit root first
+    dfs(node.left); // then left subtree
+    dfs(node.right); // then right subtree
   }
 
   dfs(root);

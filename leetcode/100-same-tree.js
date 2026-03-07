cf971ce4b75912574b3be716bf76e11546acf912
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-03-07 22:33:26 +1100

    refactor code for q.100

--- leetcode/100-same-tree.js
+++ leetcode/100-same-tree.js
@@ -15,14 +15,22 @@ var isSameTree = function (p, q) {
   //return true if p and q are identical binary tree and false if otherwise
   //BFS
   //both null, returns true
+  //   if (!p && !q) {
+  //     return true;
+  //   }
+  //   if (!p || !q) {
+  //     return false;
+  //   }
+  //   if (p.val !== q.val) {
+  //     return false;
+  //   }
+  //   return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
   if (!p && !q) {
     return true;
   }
-  if (!p || !q) {
+  if (p && q && p.val === q.val) {
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  } else {
     return false;
   }
-  if (p.val !== q.val) {
-    return false;
-  }
-  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };

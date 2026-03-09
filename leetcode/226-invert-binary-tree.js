ac2d255c6e23a34abfde5e14c646f3cc6f6628a2
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-03-09 23:52:48 +1100

    add q.226 and solution

---
+++ leetcode/226-invert-binary-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  //mirror the left branch then turn it into right branch
+  //mirror the right branch then turn it into left branch
+  //node.left, node.right = node.right,node.left
+  //create new or alter the current tree?
+  if (root === undefined || root === null) {
+    return root;
+  }
+  [root.left, root.right] = [root.right, root.left];
+  invertTree(root.left);
+  invertTree(root.right);
+  return root;
+};
